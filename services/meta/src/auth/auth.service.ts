decbc53f1fd17beb625ea81b60d38c15928c4815
Wilson <kabalisa.wilson@gmail.com>
2021-04-24 00:25:34 +0200

    refactor(meta-srv) set env for jwt secret

--- services/meta/src/auth/auth.service.ts
+++ services/meta/src/auth/auth.service.ts
@@ -1,24 +1,24 @@
 import { Injectable } from '@nestjs/common';
-import * as jwt from 'jsonwebtoken'
+import * as jwt from 'jsonwebtoken';
 
 export interface DataToken {
-  id: string
+  id: string;
 }
 
 @Injectable()
 export class AuthService {
-  private SECRET = 'azertyuiopqsdfghjklmwxcvbn1254637415247996245836'
+  private SECRET = process.env.CLOUDSHARE_JWT_SECRET;
 
   generateToken(data: DataToken): string {
-    return jwt.sign(data, this.SECRET, {expiresIn: '15m'})
+    return jwt.sign(data, this.SECRET, { expiresIn: '15m' });
   }
 
   validToken(token: string, id: string): boolean {
     try {
-      const decode = <any>jwt.verify(token, this.SECRET)
-      return decode.id == id
+      const decode = <any>jwt.verify(token, this.SECRET);
+      return decode.id == id;
     } catch (error) {
-      return false
+      return false;
     }
   }
 }
