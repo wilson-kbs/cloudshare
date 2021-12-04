a3335cacee506bb37dd95895eb1bd7d8809cd771
Wilson-kbs <kabalisa.wilson@gmail.com>
2021-12-04 01:26:33 +0100

    feat: update build stage for CI/CD

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -1,9 +1,11 @@
+const API_URL = `http://${location.host}`
+const API_BASEPATH = "/api"
 export const Config = {
-  API_URL: process.env.VUE_APP_API_URL,
-  API_BASEPATH: process.env.VUE_APP_API_BASEPATH,
-  TUS_PATH: `${process.env.VUE_APP_API_BASEPATH}/cache/`,
-  UPLOAD_PATH: `${process.env.VUE_APP_API_BASEPATH}/upload`,
-  AUTH_PATH: `${process.env.VUE_APP_API_BASEPATH}/auth`,
-  METADATA_PATH: `${process.env.VUE_APP_API_BASEPATH}/meta`,
-  DOWNLOAD_PATH: `${process.env.VUE_APP_API_BASEPATH}/d`,
+  API_URL: API_URL,
+  API_BASEPATH: API_BASEPATH,
+  TUS_PATH: `${API_BASEPATH}/cache/`,
+  UPLOAD_PATH: `${API_BASEPATH}/upload`,
+  AUTH_PATH: `${API_BASEPATH}/auth`,
+  METADATA_PATH: `${API_BASEPATH}/meta`,
+  DOWNLOAD_PATH: `${API_BASEPATH}/d`,
 };
